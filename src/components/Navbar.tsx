abc7f7767be7bf4963b7920ab80da01d2b9cf875
GabrielOchieng <ogingagabriel@gmail.com>
2024-09-11 08:10:14 +0300

    responsiveness of the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { CiSearch } from "react-icons/ci";
+// import { CiSearch } from "react-icons/ci";
 import { IoIosMenu } from "react-icons/io";
 import Sidebar from "./Sidebar";
 import { useState } from "react";
@@ -18,14 +18,14 @@ const Navbar: React.FC = () => {
             onClick={handleSideBar}
           />
         </div>
-        <div className="flex items-center w-60 p-2 border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+        {/* <div className="flex items-center w-60 p-2 border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
           <CiSearch className="h-6 w-6 text-gray-400 " />
           <input
             type="text"
             placeholder="Type to search..."
             className=" outline-none ml-2"
           />
-        </div>
+        </div> */}
         <div>
           <ThemeButton />
         </div>
